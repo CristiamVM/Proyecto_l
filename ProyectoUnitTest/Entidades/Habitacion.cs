2586911ac173aec80cb848586e010d523b477dcb
cristiam <crist@CRISTIAMV-PC>
2025-07-01 22:32:28 -0600

    Pruebas Unitarias

--- ProyectoUnitTest/Entidades/Habitacion.cs
+++ ProyectoUnitTest/Entidades/Habitacion.cs
@@ -8,7 +8,7 @@ namespace ReservasHoteles.Entidades
 {
     public class Habitacion
     {
-        public int Numero { get; set; }
+        public int? Numero { get; set; }
         public string Tipo { get; set; } // Ej: "Simple", "Doble", "Suite"
         public decimal PrecioPorNoche { get; set; }
         public bool Disponible { get; set; } = true;
